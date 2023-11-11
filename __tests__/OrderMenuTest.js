fb29980b32a517614225efc28eca6c3b2d65b0f2
skdltn210 <rhdmstn202@gmail.com>
2023-11-11 20:30:50 +0900

    test : OrderMenuTest 테스트코드 작성

---
+++ __tests__/OrderMenuTest.js
@@ -0,0 +1,19 @@
+import OrderMenu from "../src/Model/OrderMenu.js";
+
+describe("OrderMenu 클래스 테스트", () => {
+  test("calculateTotalOrderAmount 함수", () => {
+    const orderMenu = new OrderMenu({ 타파스: 2, 제로콜라: 1 });
+    const totalOrderAmount = orderMenu.calculateTotalOrderAmount();
+    expect(totalOrderAmount).toEqual(14000);
+  });
+
+  test("checkChampagneGift 함수", () => {
+    const orderMenu = new OrderMenu({ 티본스테이크: 5 });
+    expect(orderMenu.checkShampagneGift()).toEqual(true);
+  });
+
+  test("checkChampagneGift 함수", () => {
+    const orderMenu = new OrderMenu({ 타파스: 1 });
+    expect(orderMenu.checkShampagneGift()).toEqual(false);
+  });
+});
