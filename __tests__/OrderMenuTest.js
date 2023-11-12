4173b5dbc71f2aeaa73d51a15355a0e0a09c40ae
skdltn210 <rhdmstn202@gmail.com>
2023-11-12 20:35:43 +0900

    test : OrderMenuTest 테스트코드 추가

--- __tests__/OrderMenuTest.js
+++ __tests__/OrderMenuTest.js
@@ -7,13 +7,23 @@ describe("OrderMenu 클래스 테스트", () => {
     expect(totalOrderAmount).toEqual(14000);
   });
 
-  test("checkChampagneGift 함수", () => {
+  test("checkChampagneGift 함수(증정)", () => {
     const orderMenu = new OrderMenu({ 티본스테이크: 5 });
     expect(orderMenu.checkGift()).toEqual(true);
   });
 
-  test("checkChampagneGift 함수", () => {
+  test("checkChampagneGift 함수(미증정)", () => {
     const orderMenu = new OrderMenu({ 타파스: 1 });
     expect(orderMenu.checkGift()).toEqual(false);
+  });
+
+  test("countDessert 함수", () => {
+    const orderMenu = new OrderMenu({ 타파스: 1, 초코케이크: 3, 제로콜라: 2 });
+    expect(orderMenu.countDessert()).toEqual(3);
+  });
+
+  test("countDrink 함수", () => {
+    const orderMenu = new OrderMenu({ 타파스: 1, 초코케이크: 3, 제로콜라: 2 });
+    expect(orderMenu.countDrink()).toEqual(2);
   });
 });
